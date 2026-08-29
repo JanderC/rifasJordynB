08f64b2abc00527feff1a42f7840e3fee0d154bc
JanderC <janderalexisc@gmail.com>
2026-08-29 11:43:22 -0400

    cambios

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   host: process.env.DB_HOST || '200.40.68.122',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'respaldo5',
+  database: process.env.DB_NAME || 'respaldo7',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'p4ng34t3ch',
 });
